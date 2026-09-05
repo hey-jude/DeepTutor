8d9c9a95b32fd02854d335b00fde8f8fda60c298
JeongHoon Baek <coalash@gmail.com>
2026-09-05 17:46:21 +0900

    feat(web): Korean serif stack with runtime Noto Serif KR

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -43,6 +43,10 @@ module.exports = {
         ],
         serif: [
           'var(--font-serif)',
+          'Noto Serif KR',
+          'Noto Serif CJK KR',
+          'AppleMyungjo',
+          'Batang',
           'Songti SC',
           'STSong',
           'Noto Serif SC',
